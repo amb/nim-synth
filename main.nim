cff87f2abec3c4de1861b234e8141375de75f07d
Tommi Hyppänen <thyppane@gmail.com>
2024-01-06 01:42:15 +0200

    Beep boop

--- main.nim
+++ main.nim
@@ -1,4 +1,4 @@
-import raylib, std/[math, strformat]
+import raylib, std/[math, strformat, sets]
 
 const
     screenWidth = 800
@@ -9,97 +9,85 @@ const
     MaxSamplesPerUpdate = 4096
 
 var
-    frequency: float32 = 440 # Cycles per second (hz)
-    audioFrequency: float32 = 440 # Audio frequency, for smoothing
-    oldFrequency: float32 = 1 # Previous value, used to test if sine needs to be rewritten, and to smoothly modulate frequency
-    sineIdx: float32 = 0 # Index for audio rendering
+    frequency: float32 = 440
+    sineIdx: float32 = 0
 
 proc audioInputCallback(buffer: pointer; frames: uint32) {.cdecl.} =
-    # Audio input processing callback
-    audioFrequency = frequency + (audioFrequency - frequency)*0.95'f32
-    audioFrequency += 1
-    audioFrequency -= 1
-    let incr = audioFrequency/44100'f32
+    let incr = frequency / 44100'f32
     let d = cast[ptr UncheckedArray[int16]](buffer)
     for i in 0..<frames:
-        d[i] = int16(32000'f32*sin(2*PI*sineIdx))
+        d[i] = int16(32000'f32 * sin(2 * PI * sineIdx))
         sineIdx += incr
         if sineIdx > 1: sineIdx -= 1
+
+proc getActiveNotes(): seq[int] =
+    const musicKeys1 = [
+        KeyboardKey.Q, KeyboardKey.Two, KeyboardKey.W, KeyboardKey.Three, KeyboardKey.E, KeyboardKey.R, 
+        KeyboardKey.Five, KeyboardKey.T, KeyboardKey.Six, KeyboardKey.Y, KeyboardKey.Seven, KeyboardKey.U,
+        KeyboardKey.I, KeyboardKey.Nine, KeyboardKey.O, KeyboardKey.Zero, KeyboardKey.P
+    ]
+
+    const musicKeys2 = [
+        KeyboardKey.Z, KeyboardKey.S, KeyboardKey.X, KeyboardKey.D, KeyboardKey.C, KeyboardKey.V,
+        KeyboardKey.G, KeyboardKey.B, KeyboardKey.H, KeyboardKey.N, KeyboardKey.J, KeyboardKey.M,
+        KeyboardKey.Comma, KeyboardKey.L, KeyboardKey.Period
+    ]
+
+    for mk_id, mkey in musicKeys1:
+        if isKeyDown(mkey):
+            result.add(mk_id)
+
+    for mk_id, mkey in musicKeys2:
+        if isKeyDown(mkey):
+            result.add(mk_id - 12)
 
 proc main =
     # Initialization
     initWindow(screenWidth, screenHeight, "raylib [audio] example - raw audio streaming")
-    defer: closeWindow() # Close window and OpenGL context
+    defer: closeWindow()
 
-    initAudioDevice() # Initialize audio device
-    defer: closeAudioDevice() # Close audio device (music streaming is automatically stopped)
+    var fontPixantiqua = loadFont("res/pixantiqua.ttf")
+
+    initAudioDevice()
+    defer: closeAudioDevice()
 
     setAudioStreamBufferSizeDefault(MaxSamplesPerUpdate)
 
-    # Init raw audio stream (sample rate: 44100, sample size: 16bit-short, channels: 1-mono)
-    var stream = loadAudioStream(44100, 16, 1)
+    var stream = loadAudioStream(48000, 16, 1)
     setAudioStreamCallback(stream, audioInputCallback)
 
     # Buffer for the single cycle waveform we are synthesizing
     var data = newSeq[int16](MaxSamples)
 
-    # Frame buffer, describing the waveform when repeated over the course of a frame
-    var writeBuf = newSeq[int16](MaxSamplesPerUpdate)
     playAudioStream(stream)
 
-    var mousePosition = Vector2(x: -100, y: -100)
-    var waveLength: int32 = 1
-    var position: Vector2
-
-    setTargetFPS(30) # Set our game to run at 30 frames-per-second
-    while not windowShouldClose(): # Detect window close button or ESC key
-        mousePosition = getMousePosition()
+    setTargetFPS(30)
+    while not windowShouldClose():
+        var mousePosition = getMousePosition()
         if isMouseButtonDown(Left):
             let fp = mousePosition.y
             frequency = 40 + fp
             let pan = mousePosition.x/screenWidth
             setAudioStreamPan(stream, pan)
-        if frequency != oldFrequency:
-            # Compute wavelength. Limit size in both directions.
-            # let oldWavelength = waveLength
-            waveLength = int32(22050/frequency)
-            if waveLength > MaxSamples div 2: waveLength = MaxSamples div 2
-            if waveLength < 1: waveLength = 1
-            for i in 0..<waveLength*2:
-                data[i] = int16(sin(2*PI*i.float32/waveLength.float32)*32000)
-            # Make sure the rest of the line is flat
-            for j in waveLength*2..<MaxSamples:
-                data[j] = int16(0)
-            # Scale read cursor's position to minimize transition artifacts
-            # readCursor = int32(readCursor.float32*(waveLength.float32/oldWavelength.float32))
-            oldFrequency = frequency
 
-        # # Refill audio stream if required
-        # if isAudioStreamProcessed(stream):
-        #     # Synthesize a buffer that is exactly the requested size
-        #     var writeCursor: int32 = 0
-        #     while writeCursor < MaxSamplesPerUpdate:
-        #         # Start by trying to write the whole chunk at once
-        #         var writeLength = MaxSamplesPerUpdate - writeCursor
-        #         # Limit to the maximum readable size
-        #         let readLength = waveLength - readCursor
-        #         if writeLength > readLength: writeLength = readLength
-        #         copyMem(addr writeBuf[writeCursor], addr data[readCursor], writeLength*sizeof(int16))
-        #         # Update cursors and loop audio
-        #         readCursor = (readCursor + writeLength) mod waveLength
-        #         inc writeCursor, writeLength
-        #     # Copy finished frame to audio stream
-        #     updateAudioStream(stream, writeBuf)
+        for n in getActiveNotes():
+            frequency = 440 * pow(2, (n-12).float32/12)
+
+        if frequency < 0: frequency = 0
 
         beginDrawing()
         clearBackground(RayWhite)
-        drawText(&"sine frequency: {frequency.int32}", getScreenWidth() - 220, 10, 20, Red)
-        drawText("click mouse button to change frequency or pan", 10, 10, 20, DarkGray)
+
+        var textOut = (fmt"Frequency: {frequency.int32}").cstring
+        drawText(fontPixantiqua, textOut, Vector2(x: 10.0 , y: 10.0), fontPixantiqua.baseSize.float32, 4.0, Red)
+
         # Draw the current buffer state proportionate to the screen
         for i in 0..<screenWidth:
+            var position: Vector2
             position.x = float32(i)
-            position.y = 250 + 50*data[i*MaxSamples div screenWidth].float32/32000
-            drawPixel(position, Red)
+            position.y = 100 + 50*data[i*MaxSamples div screenWidth].float32/32000
+            drawPixel(position.x.int32, position.y.int32, Red)
+            drawPixel(position.x.int32, position.y.int32 + 1, Red)
         endDrawing()
 
 main()
