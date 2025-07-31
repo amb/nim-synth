b365232800fb28bfff1f626a6d2da4af35116cb4
Tommi Hyppänen <thyppane@gmail.com>
2025-07-31 15:00:18 +0300

    FreeVerb-ish

--- synth/components/combfilter.nim
+++ synth/components/combfilter.nim
@@ -4,7 +4,11 @@ type CombFilter*[L: static[int]] = object
     buffer: RingBuffer[L, float32]
     delay*: uint32
     feedBack*: float32
+    damping*: float32
+    filterStore: float32
 
 proc render*(comb: var CombFilter, input: float32): float32 =
-    result = input + comb.feedBack * comb.buffer.read(comb.delay)
+    # TODO: possible un-denormalize macro etc. if performance issues
+    comb.filterStore = comb.filterStore * comb.damping + comb.buffer.read(comb.delay) * (1.0'f32 - comb.damping)
+    result = input + comb.feedBack * comb.filterStore
     comb.buffer.write(result)
--- synth/components/reverb.nim
+++ synth/components/reverb.nim
@@ -1,81 +1,119 @@
-import std/[math, bitops]
-import ringbuf, allpass, combfilter
+import std/math
+import allpass, combfilter
 
-const rAllPasses = [480.0, 0.7, 161.0, 0.7, 46.0, 0.7]
-const rCombFilters = [3460.0, 0.805, 2988.0, 0.827, 3882.0, 0.783, 4312.0, 0.764]
+const rCombTuning = [1116, 1188, 1277, 1356, 1422, 1491, 1557, 1617]
+const rAllPassTuning = [556, 441, 341, 225]
+
+const stereoSpread = 23
+const fixedGain = 0.015'f32
+const scaleWet = 3
+const scaleDry = 2
+const scaleDamp = 0.4'f32
+const scaleRoom = 0.28'f32
+const offsetRoom = 0.7'f32
+
+const NUM_ALLPASS = 4
+const NUM_COMB = 8
+
+type ReverbChannel = object
+    combf: array[NUM_COMB, CombFilter[2048]]
+    allp: array[NUM_ALLPASS, AllPass[1024]]
 
 type Reverb* = object
-    combf: array[4, CombFilter[8192]]
-    allp: array[3, AllPass[512]]
-    dry: float32
+    left, right: ReverbChannel
+    roomsize, damp, wet, dry, width, mode: float32
+    wet1, wet2, gain, roomsize1, damp1: float32
+
+proc newReverbChannel(delay: uint16): ReverbChannel =
+    result = ReverbChannel()
+
+    for i in 0..<NUM_ALLPASS:
+        result.allp[i].delay = rAllPassTuning[i].uint16 + delay
+
+    for i in 0..<NUM_COMB:
+        result.combf[i].delay = rCombTuning[i].uint16 + delay
+
+proc update(self: var Reverb) =
+    self.wet1 = self.wet * (self.width / 2'f32 + 0.5f)
+    self.wet2 = self.wet * ((1'f32 - self.width) / 2'f32)
+
+    self.roomsize1 = self.roomsize
+    self.damp1 = self.damp
+    self.gain = fixedGain
+
+    for i in 0..<NUM_COMB:
+        self.left.combf[i].feedBack = self.roomsize1
+        self.right.combf[i].feedBack = self.roomsize1
+
+    for i in 0..<NUM_COMB:
+        self.left.combf[i].damping = self.damp1
+        self.right.combf[i].damping = self.damp1
+
+proc setRoomSize(self: var Reverb, value: float32) =
+    self.roomsize = (value * scaleRoom) + offsetRoom
+    self.update()
+
+proc getRoomSize(self: Reverb): float32 =
+    (self.roomsize - offsetRoom) / scaleRoom
+
+proc setDamp(self: var Reverb, value: float32) =
+    self.damp = value * scaleDamp
+    self.update()
+
+proc getDamp(self: Reverb): float32 =
+    self.damp / scaleDamp
+
+proc setWet(self: var Reverb, value: float32) =
+    self.wet = value * scaleWet
+    self.update()
+
+proc getWet(self: Reverb): float32 =
+    self.wet / scaleWet
+
+proc setDry(self: var Reverb, value: float32) =
+    self.dry = value * scaleDry
+
+proc getDry(self: Reverb): float32 =
+    self.dry / scaleDry
+
+proc setWidth(self: var Reverb, value: float32) =
+    self.width = value
+    self.update()
+
+proc getWidth(self: Reverb): float32 =
+    self.width
 
 proc newReverb*(): Reverb =
     var reverb = Reverb()
-    reverb.dry = 0.5
+    reverb.left = newReverbChannel(0)
+    reverb.right = newReverbChannel(stereoSpread)
 
-    for i in 0..2:
-        reverb.allp[i].delay = rAllPasses[i * 2].uint16
-        reverb.allp[i].ratio = rAllPasses[i * 2 + 1]
+    reverb.roomsize = 0.5'f32
+    reverb.damp = 0.5'f32
+    reverb.wet = 1'f32/scaleWet
+    reverb.dry = 0
+    reverb.width = 1
 
-    for i in 0..3:
-        reverb.combf[i].delay = rCombFilters[i * 2].uint16
-        reverb.combf[i].feedBack = rCombFilters[i * 2 + 1]
+    reverb.setRoomSize(0.9)
+    reverb.setDamp(0.99)
 
     return reverb
 
-proc render*(reverb: var Reverb, input: float32): float32 =
-    var sample: float32 = input
+proc renderStereo*(self: var Reverb, input: float32): (float32, float32) =
+    var outL, outR: float32
 
-    for i in 0..2:
-        sample = reverb.allp[i].render(sample)
+    # Comb filters in parallel
+    for i in 0..<NUM_COMB:
+        outL += self.left.combf[i].render(input)
+        outR += self.right.combf[i].render(input)
 
-    let cval = sample
-    sample = 0.0
-    for i in 0..3:
-        sample += reverb.combf[i].render(cval)
-    sample *= 0.25
+    # Allpass filters in series
+    for i in 0..<NUM_ALLPASS:
+        outL = self.left.allp[i].render(outL)
+        outR = self.right.allp[i].render(outR)
 
-    return reverb.dry * input + (1.0 - reverb.dry) * sample
+    # Mix output
+    let lc = outL * self.wet1 + outR * self.wet2 + input * self.dry
+    let rc = outR * self.wet1 + outL * self.wet2 + input * self.dry
 
-proc renderStereo*(reverb: var Reverb, input: float32): (float32, float32) =
-    # https://ccrma.stanford.edu/~jos/pasp/Schroeder_Reverberators.html
-    var sample: float32 = input
-
-    # Allpass block
-    for i in 0..2:
-        sample = reverb.allp[i].render(sample)
-
-    # Comb filter block
-    let c1 = reverb.combf[0].render(sample)
-    let c2 = reverb.combf[1].render(sample)
-    let c3 = reverb.combf[2].render(sample)
-    let c4 = reverb.combf[3].render(sample)
-
-    # Mixing matrix
-    let s1 = c1 + c3
-    let s2 = c2 + c4
-
-    let oa = s1 + s2
-    let od = s1 - s2
-    # let ob = -oa
-    # let oc = -od
-
-    # Stereo out
-    var left = oa
-    var right = od
-
-    left = reverb.dry * input + (1.0 - reverb.dry) * left
-    right = reverb.dry * input + (1.0 - reverb.dry) * right
-    return (left, right)
-
-if isMainModule:
-    echo "Reverb test"
-
-    var reverb = newReverb()
-    var input: float32 = 0.0
-    var output: float32 = 0.0
-
-    for i in 0..10:
-        input = sin(i.float32 / 10.0)
-        output = reverb.render(input)
-        echo output
+    return (input * self.gain + lc, input * self.gain + rc)
